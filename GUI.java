4bbf8558448ae98cc8c4f777cbc075d6b7e8a159
19974608 <155991545+19974608@users.noreply.github.com>
2024-03-13 22:10:49 -0700

    Lesson 8
    lesson 8

--- GUI.java
+++ GUI.java
@@ -1,68 +1,84 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
-public class GUI extends JFrame {
+public class GUI extends JFrame implements ActionListener {
         private JTextArea messageArea;
         private JCheckBox saveMessagesCheckBox;
 
         public GUI() {
-                setTitle("Game Application");
+                setTitle("Game Menu GUI");
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setSize(400, 300);
+
                 JMenuBar menuBar = new JMenuBar();
-                setJMenuBar(menuBar);
+
                 JMenu gameMenu = new JMenu("Game");
-                gameMenu.setMnemonic('G'); // Alt-G shortcut
-                menuBar.add(gameMenu);
+                gameMenu.setMnemonic(KeyEvent.VK_G);
+
                 JMenuItem addPlayerMenuItem = new JMenuItem("Add Player");
-                addPlayerMenuItem.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                        }
-                });
+                addPlayerMenuItem.addActionListener(this);
                 gameMenu.add(addPlayerMenuItem);
+
                 JMenuItem addHostMenuItem = new JMenuItem("Add Host");
-                addHostMenuItem.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                        }
-                });
+                addHostMenuItem.addActionListener(this);
                 gameMenu.add(addHostMenuItem);
+
+                menuBar.add(gameMenu);
                 JMenu aboutMenu = new JMenu("About");
-                aboutMenu.setMnemonic('A'); // Alt-A shortcut
-                menuBar.add(aboutMenu);
+                aboutMenu.setMnemonic(KeyEvent.VK_A);
+
                 JMenuItem layoutMenuItem = new JMenuItem("Layout");
-                layoutMenuItem.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                                JOptionPane.showMessageDialog(GUI.this, "Description of chosen layout...");
-                        }
-                });
+                layoutMenuItem.addActionListener(this);
                 aboutMenu.add(layoutMenuItem);
-                JPanel mainPanel = new JPanel();
-                mainPanel.setLayout(new BorderLayout());
-                add(mainPanel);
+
+                menuBar.add(aboutMenu);
+
+                setJMenuBar(menuBar);
+                JPanel mainPanel = new JPanel(new BorderLayout());
                 messageArea = new JTextArea();
                 messageArea.setEditable(false);
                 JScrollPane scrollPane = new JScrollPane(messageArea);
                 mainPanel.add(scrollPane, BorderLayout.CENTER);
                 saveMessagesCheckBox = new JCheckBox("Save Messages");
                 saveMessagesCheckBox.setToolTipText("Check to save messages");
+                saveMessagesCheckBox.addActionListener(this);
                 mainPanel.add(saveMessagesCheckBox, BorderLayout.SOUTH);
-                saveMessagesCheckBox.setToolTipText("Check to save messages history");
 
-                setSize(400, 300);
-                setLocationRelativeTo(null);
+                add(mainPanel);
+
+                setVisible(true);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+                String command = e.getActionCommand();
+
+                if (command.equals("Add Player")) {
+                        appendMessage("Player added.");
+                } else if (command.equals("Add Host")) {
+                        appendMessage("Host added.");
+                } else if (command.equals("Layout")) {
+                        JOptionPane.showMessageDialog(this, "I chose this layout for its simplicity and ease of use.");
+                } else if (e.getSource() == saveMessagesCheckBox) {
+                        if (saveMessagesCheckBox.isSelected()) {
+                                appendMessage("Save messages checked.");
+                        } else {
+                                messageArea.setText("");
+                                appendMessage("Save messages unchecked.");
+                        }
+                }
+        }
+
+        private void appendMessage(String message) {
+                if (saveMessagesCheckBox.isSelected()) {
+                        messageArea.append(message + "\n");
+                } else {
+                        messageArea.setText(message + "\n");
+                }
         }
 
         public static void main(String[] args) {
-                SwingUtilities.invokeLater(new Runnable() {
-                        @Override
-                        public void run() {
-                                GUI gui = new GUI();
-                                gui.setVisible(true);
-                        }
-                });
+                SwingUtilities.invokeLater(() -> new GUI());
         }
 }
